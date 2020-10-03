122e5fb22e7109db3b0f8df39bdace26d1c98f85
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-03 21:49:17 +0530

    [tejas] add out method practice

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+      public  static void Main(string[] args)
+        {
+            int SumIs = 0;
+            int ProdIs = 0;
+
+            int a = 0;
+            Console.WriteLine("Enter first Number");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            int b = 0;
+            Console.WriteLine("Enter second Number");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Caclculate(a,b, out SumIs, out ProdIs);
+            Console.WriteLine("The sum of 2 numbers is "+ SumIs);
+            Console.WriteLine("The product of 2 numbers is "+ProdIs);
+
+
+
+        }
+        public static void Caclculate(int num1, int num2, out int Sum, out int Prod)
+        {
+            Sum = num1 + num2;
+            Prod = num1 * num2;
+        }
+           
+    }
+}
